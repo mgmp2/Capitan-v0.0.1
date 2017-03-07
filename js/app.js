9cce5d9aad027d3c38f14af44d833773403a3746
Miriam Mendoza <miriammp1997@gmail.com>
2017-03-07 14:32:21 -0500

    Casi terminado

--- js/app.js
+++ js/app.js
@@ -91,3 +91,22 @@ sprint3.style.display = "none";
       alert("Debe responder todas las preguntas")
 
     }};
+
+    //dropdown
+    function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
